b69759af7609fcceb2e083e4d04391ff2876dced
Jonathan Adami <pitilezard@gmail.com>
2021-08-20 13:05:05 +1000

    basic app skeleton

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,25 @@
+import { Fragment } from "react";
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 
 const App = (props) => {
-    return <Button>Test</Button>;
+    return (
+        <Fragment>
+            <Grid container>
+                <Grid item xs={3}>
+                    Here the smeter
+                </Grid>
+                <Grid item xs={6}>
+                    Here frequency/vfos stuff like that
+                </Grid>
+                <Grid item xs={3}>
+                    Here buttons
+                </Grid>
+            </Grid>
+            <div>Here the frequency</div>
+            <div>Here the waterfall</div>
+        </Fragment>
+    );
 };
 
 export default App;
